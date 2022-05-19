7c40ea86473eda817df4b4c4168fc4f0fd08e783
maftunamahammadiyeva <mahammadiyevamaftuna01@gmail.com>
2022-05-19 22:59:15 +0500

    initial

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var mamlakat = "Usbekistan";
+var continent = "448,978 km2";
+var population = "3200000";
+
+console.log(mamlakat, continent, population );
+
+var isIsland = "Germany";
+var language;
+
+console.log(typeof mamlakat);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
+
+var language ="o'zbek";
+var firstName = prompt ("Ismingizni kiriting");
+var lastName = prompt ("Familyangizni kiriting");
+var userAge = prompt ("Yoshingiz");
+var country = prompt ("Mamlakatingiz");
+var tilingiz = prompt ("Tilingiz")
+console.log( firstName, lastName,userAge,tilingiz);
